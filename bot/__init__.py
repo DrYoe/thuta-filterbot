d71b1cf0526d8c8b3a9a8cc9000e0432683ffe3a
DrYoe <100612710+DrYoe@users.noreply.github.com>
2023-01-23 22:17:09 +0630

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,15 +11,15 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int("1234")
+APP_ID = int("12158462")
 
-API_HASH = "1234"
+API_HASH = "0b962717d931f4480c46d56c85d409a5"
 
-BOT_TOKEN = "1234"
+BOT_TOKEN = "5618135104:AAG0gwyVVffpXrYqEqRGBXAy6co5lglk0kA"
 
-DB_URI = "1234"
+DB_URI = "mongodb+srv://thutafilter:thutafilter@thuta2023.znq8uex.mongodb.net/?retryWrites=true&w=majority"
 
-USER_SESSION = "1234"
+USER_SESSION = "BQBkx2v2hjsupXAUHFzXjVq_oUNYLGmse8khbRuJz59pbeczR4z2N7KXEIytxJ8oR8BnzPseJ-J5QaHUVTwp2T7-SgqVIwqdMCyeycdSfVKzLQeSH1NvNJ_T-YUtDoT-CNOh91qlsxWsVw3IZgJTMX8usaPA0pKjdRn6BkcwmBwgTd_qPCfeWDfupreY2Ha_xCBft1pRews1_28ogcs9unU8dsfVLTg63d7y3OI1U7T3Wp825TcAtTNgO_xcn7q7Av09jJctPw0OOpMX6FDxgK4YKerpFmm7PkMDFm-aBsUrlgy2tbHFG6zlliZeNFpWq3Mo_4HZ_zlD0FCHZbNDrbS6UFsxVQA"
 
 VERIFY = {}
 
